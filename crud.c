71069ce80266437746da0d5643133792f004fdee
vinci <davinlsj@gmail.com>
2023-05-09 22:28:38 +0900

    revise

--- crud.c
+++ crud.c
@@ -7,15 +7,15 @@ void standardChecker(standard *s){
     printf("당신이 원하는 전공공부 시간은?");
     scanf("%d",&s->majorStudy);
     printf("당신이 원하는 전공 외 공부 시간은?");
-    scanf("%d",&s->minorStudy);
+    scanf("%d",&s->otherStudy);
     printf("당신이 원하는 수면 시간은?");
     scanf("%d",&s->sleepTime);
     printf("당신이 원하는 식사 횟수는?");
-    scanf("%d",&s->eatingCount);
+    scanf("%d",&s->mealCount);
     printf("당신이 원하는 독서시간은?");
     scanf("%d",&s->readingTime);
     printf("당신이 원하는 사교시간은?");
-    scanf("%d",&s->friendshipTimme);
+    scanf("%d",&s->friendshipTime);
 }
 int menu(){
     int select;
@@ -25,18 +25,20 @@ int menu(){
     printf("4. 원하는 날짜의 갓생데이터를 수정해 보자!");
     printf("5. 원하는 날짜의 갓생을 확인해보자!");
     printf("6. 일일날짜의 ");
+
+    //return select;
 }
 void saveData(standard *s,daily *d[]  ,int count){
     int i = 0 ;
     FILE *fp;
     fp = fopen("standardText.txt", "wt");
-    fprintf(fp,"%s %s %s %s %s %s %s \n",s->exerciseTime, s->majorStudy, s->minorStudy,s->sleepTime, s->eatingCount,s->readingTime,s->friendshipTime );
+    fprintf(fp,"%s %s %s %s %s %s %s \n",s->exerciseTime, s->majorStudy, s->otherStudy,s->sleepTime, s->mealCount,s->readingTime,s->friendshipTime );
     fclose(fp);
 
     fp = fopen("DailyData.txt", "wt");
     for(; i < count ; i ++){
         if(s[i]== NULL)continue;
-        fprintf(fp,"%s %s %s %s %s %s %s %s \n", d[i]->dailycount, d[i]->exerciseTime,d[i]->majorStudy, d[i]->minorStudy,d[i]->sleepTime, d[i]->eatingCount,d[i]->readingTime,d[i]->friendshipTime);
+        fprintf(fp,"%s %s %s %s %s %s %s %s \n", d[i]->day, d[i]->exerciseTime,d[i]->majorStudy, d[i]->otherStudy,d[i]->sleepTime, d[i]->mealCount,d[i]->readingTime,d[i]->friendshipTime);
     }
     fclose(fp);
     printf("SAVE!!\n");
@@ -55,22 +57,22 @@ int loadData(standard *s, daily *d[]){
         fscanf(fp,"%d",&s->exerciseTime);
         if(feof(fp))break;
         fscanf(fp,"%d",&s->majorStudy);
-        fscanf(fp,"%d",&s->minorStudy);
+        fscanf(fp,"%d",&s->otherStudy);
         fscanf(fp,"%d",&s->sleepTime);
-        fscanf(fp,"%d",&s->eatingCount);
+        fscanf(fp,"%d",&s->mealCount);
         fscanf(fp,"%d",&s->readingTime);
         fscanf(fp,"%d",&s->friendshipTime);
     }
     fp = fopen("DailyData.txt", "rt");
     for(; i < 100; i++){
         d[i] = (daily*)malloc(sizeof(daily));
-        fscanf(fp,"%d",&d[i]->dailycount);
+        fscanf(fp,"%d",&d[i]->day);
         if(feof(fp))break;
         fscanf(fp,"%d",&d[i]->exerciseTime);
         fscanf(fp,"%d",&d[i]->majorStudy);
-        fscanf(fp,"%d",&d[i]->minorStudy);
+        fscanf(fp,"%d",&d[i]->otherStudy);
         fscanf(fp,"%d",&d[i]->sleepTime);
-        fscanf(fp,"%d",&d[i]->eatingCount);
+        fscanf(fp,"%d",&d[i]->mealCount);
         fscanf(fp,"%d",&d[i]->readingTime);
         fscanf(fp,"%d",&d[i]->friendshipTime);
     }
--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include "crud.h"
 #include "evaluate.h"
+
+
+
+int main(void) {
+    int menu; //사용자가 선택한 메뉴번호
+
+    while(1) {
+        
+    }
+}

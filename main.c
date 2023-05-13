9ce402bf8761d9ecd3a8e08065ef71117c50ad41
vinci <davinlsj@gmail.com>
2023-05-13 23:52:25 +0900

    5_13_2

--- main.c
+++ main.c
@@ -22,38 +22,33 @@ int main(void) {
             //갓생 기준 설정/수정하기
             standardChecker(myStandard);
         }
-        else if (count != 0) {
-            else if (menuNum == 2) {
-                //일일 데이터 입력
-                myDaily[count] = (daily*)malloc(sizeof(daily)); //일일데이터 저장공간 메모리 할당
-                addDailyData(myStandard, myDaily, count);
-                count ++;
-            }
-        
-            else if (menuNum == 3) {
-                //전체 일일 데이터 삭제
-                result = deleteDailyData(myDaily, count);
-                if (result ==  1) count = 0; //삭제되었다면 count를 0으로 변환
-            }
-            else if (menuNum == 4) {
-                //일일 데이터 수정
-                updateDailyData(myDaily, count);
-            }
-            else if (menuNum == 5) {
-                //일일 데이터 출력
-                showDailyData(myDaily, count);
-            }
-            else if (menuNum == 6) {
-                //일별 갓생 평가
-                conformDaily(myStandard, myDaily);
-            }
-            else if (menuNum == 7) {
-                //주별 갓생 평가
-                calculatorWeek(myStandard);
-            }
+        else if (menuNum == 2) {
+            //일일 데이터 입력
+            myDaily[count] = (daily*)malloc(sizeof(daily)); //일일데이터 저장공간 메모리 할당
+            addDailyData(myStandard, myDaily, count);
+            count ++;
         }
-        else {
-            printf("");
+    
+        else if (menuNum == 3) {
+            //전체 일일 데이터 삭제
+            result = deleteDailyData(myDaily, count);
+            if (result ==  1) count = 0; //삭제되었다면 count를 0으로 변환
+        }
+        else if (menuNum == 4) {
+            //일일 데이터 수정
+            updateDailyData(myDaily, count);
+        }
+        else if (menuNum == 5) {
+            //일일 데이터 출력
+            showDailyData(myDaily, count);
+        }
+        else if (menuNum == 6) {
+            //일별 갓생 평가
+            conformDaily(myStandard, myDaily);
+        }
+        else if (menuNum == 7) {
+            //주별 갓생 평가
+            calculatorWeek(myStandard);
         }
         else if (menuNum == 8) {
             //데이터 저장
